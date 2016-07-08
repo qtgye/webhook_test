297cdb6334ee22458744a2cfeb4794276190ef9a
Jayson Buquia <jayson.buquia@codeandtheory.com>
2016-07-08 20:39:58 +0800

    Debug

--- deploy.php
+++ deploy.php
@@ -3,22 +3,25 @@
 $post = $_POST;
 $command = 'git pull origin 2>&1';
 
-// if ( $post && $post["payload"] ) {
-// 	$output = shell_exec($command);
-// 	echo '<pre style="display: table; font-size: 10px">';
-// 		var_dump($output);
-// 	echo '</pre>';
+echo '<pre style="display: table; font-size: 10px">';
+	var_dump($post);
+echo '</pre>';
 
-// }
+if ( $post && $post["payload"] ) {
+	$output = shell_exec($command);
+	echo $output;
+	return;
+}
+
+echo 'Unauthorized deployment';
+echo '<pre style="display: table; font-size: 10px">';
+	var_dump($post);
+echo '</pre>';
+
 // echo '<pre style="display: table; font-size: 10px">';
 // 	var_dump($post["payload"]);
 // echo '</pre>';
 
-	$output = shell_exec($command);
-	echo '<pre style="display: table; font-size: 10px">';
-		var_dump($output);
-	echo '</pre>';
-	
 
 ?>
 
