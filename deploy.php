190bcd7027438ca1a5fac945ba975b5a65a6155d
Jayson Buquia <jayson.buquia@codeandtheory.com>
2016-07-08 20:04:59 +0800

    Run shell without payload check

--- deploy.php
+++ deploy.php
@@ -1,18 +1,23 @@
 <?php 
 
 $post = $_POST;
-$command = 'git pull';
+$command = 'git pull origin 2>&1';
 
-if ( $_POST && $_POST['payload'] ) {
+// if ( $post && $post["payload"] ) {
+// 	$output = shell_exec($command);
+// 	echo '<pre style="display: table; font-size: 10px">';
+// 		var_dump($output);
+// 	echo '</pre>';
+
+// }
+// echo '<pre style="display: table; font-size: 10px">';
+// 	var_dump($post["payload"]);
+// echo '</pre>';
+
 	$output = shell_exec($command);
 	echo '<pre style="display: table; font-size: 10px">';
 		var_dump($output);
 	echo '</pre>';
-
-}
-echo '<pre style="display: table; font-size: 10px">';
-	var_dump($post["payload"]);
-echo '</pre>';
 
 ?>
 
