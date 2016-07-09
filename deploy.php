13da37ff88673db6a39111d16ef0d5cb777fbebd
Jayson Buquia <jayson.buquia@codeandtheory.com>
2016-07-09 10:31:37 +0800

    test commit

--- deploy.php
+++ deploy.php
@@ -18,10 +18,5 @@ echo '<pre style="display: table; font-size: 10px">';
 	var_dump($post);
 echo '</pre>';
 
-// echo '<pre style="display: table; font-size: 10px">';
-// 	var_dump($post["payload"]);
-// echo '</pre>';
-
-
 ?>
 
